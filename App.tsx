8c6c22d2efab3f442fadbbb6488d90b1a6ee8ac4
taeuk-kang <taeuk_kang@naver.com>
2020-10-29 00:27:55 +0900

    navigation - auth, home

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import * as WebBrowser from 'expo-web-browser';
 import * as firebase from 'firebase';
-import { makeRedirectUri, useAuthRequest, getDefaultReturnUrl, } from 'expo-auth-session';
-import { StyleSheet, View, Button, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthScreen } from './src/screens/auth/Auth';
+import { HomeScreen } from './src/screens/home/Home';
 
-WebBrowser.maybeCompleteAuthSession();
 
-// FIXME: 배포시에는 REDIRECT_URL 제거하고, native에 맞춰 수정
-// 최종적으로는 development 모드에 따라 설정되게 짜놓는게 좋을 듯
-
-const REDIRECT_URL = getDefaultReturnUrl();
 const firebaseConfig = {
   apiKey: "AIzaSyCPb5d46YfLqY4mdaQJHRKMum_oMU-6T40",
   authDomain: "dailycommit-cd5a0.firebaseapp.com",
@@ -21,56 +17,21 @@ const firebaseConfig = {
   measurementId: "G-MF6HZ4RCQ4"
 };
 
-const discovery = {
-  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-  tokenEndpoint: 'https://github.com/login/oauth/access_token',
-  revocationEndpoint: 'https://github.com/settings/connections/applications/7f6d75fe84b0b867318e',
-};
-
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-export default function App() {
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      clientId: '7f6d75fe84b0b867318e',
-      scopes: ['identity'],
-      redirectUri: REDIRECT_URL || makeRedirectUri({       
-        // For usage in bare and standalone
-        native: 'comdailycommit://redirect',
-      }),
-    },
-    discovery
-  );
-  
-  React.useEffect(() => {
-    console.log(response);
-    if (response?.type === 'success') {
-      const { code } = response.params;
+const Stack = createStackNavigator();
 
-      console.log(code);
-    }
-  }, [response]);
-
+export default function App() {  
   return (
-    <View style={styles.container}>
-      <Button
-        disabled={!request}
-        title="Login TEST"
-        onPress={() => {
-          promptAsync();
-        }}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Auth" headerMode="none">
+        <Stack.Screen name="Auth" component={AuthScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/home/Home.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParamList } from '../../shared-interfaces';
+
+export type Props = StackScreenProps<RootStackParamList, 'Home'>;
+
+export function HomeScreen({ route, navigation }: Props ) {
+  const { token } = route.params;
+  console.log(token);
+
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
